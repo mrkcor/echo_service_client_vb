8a06aa75e170d3e9dc63d74eeeadd125a52c9236
Mark Kremer <mark@without-brains.net>
2011-10-30 06:40:35 +0100

    Initial commit

---
+++ EchoClient1/Module1.vb
@@ -0,0 +1,20 @@
+﻿Module Module1
+
+    Dim client As Echo.EchoPortTypeClient
+    Dim request As Echo.EchoMessageType
+    Dim response As Echo.EchoMessageType
+    Dim message As String
+
+    Sub Main()
+        message = Command()
+        If message = "" Then
+            message = "Hello from VisualBasic"
+        End If
+        client = New Echo.EchoPortTypeClient()
+        request = New Echo.EchoMessageType()
+        request.Message = message
+        response = client.Echo(request)
+        Console.WriteLine(response.Message)
+    End Sub
+
+End Module
