b54d0c0ec3e137bc39e2f0b1fd428939699cd25e
Mark Kremer <mark@without-brains.net>
2011-11-06 17:37:21 +0100

    Added explanatory comments

--- EchoClient1/Module1.vb
+++ EchoClient1/Module1.vb
@@ -6,29 +6,39 @@
     Dim message As String
 
     Sub Main()
+        ' If command line arguments were given then they are used
+        ' as message to the EchoService
         message = Command()
 
+        ' If no command line arguments were given then default the
+        ' message to "Hello from VisualBasic"
         If message = "" Then
             message = "Hello from VisualBasic"
         End If
 
+        ' Instantiate new EchoService client
         client = New Echo.EchoPortTypeClient()
 
+        ' Instantiate the request object and set the message within it
         request = New Echo.EchoMessageType()
         request.Message = message
 
         Try
+            ' Call Echo and output the result
             response = client.Echo(request)
             Console.WriteLine("EchoService responded to Echo: " + response.Message)
-        Catch sf As System.ServiceModel.FaultException
-            Console.WriteLine("SOAP fault: " + sf.Message)
+        Catch exception As System.ServiceModel.CommunicationException
+            ' In case of an error (SOAP fault or otherwise) output the error
+            Console.WriteLine("An error occurred while calling Echo on the EchoService: " + exception.Message)
         End Try
 
         Try
+            ' Call ReverseEcho and output the result
             response = client.ReverseEcho(request)
             Console.WriteLine("EchoService responded to ReverseEcho: " + response.Message)
-        Catch sf As System.ServiceModel.FaultException
-            Console.WriteLine("SOAP fault: " + sf.Message)
+        Catch exception As System.ServiceModel.CommunicationException
+            ' In case of an error (SOAP fault or otherwise) output the error
+            Console.WriteLine("An error occurred while calling ReverseEcho on the EchoService: " + exception.Message)
         End Try
     End Sub
 
